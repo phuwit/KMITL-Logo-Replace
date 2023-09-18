b100494953ff6e9aa68ca5d36dd907a893fbc8a2
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2023-09-18 11:04:16 +0700

    edited logo svgs to finally have colors

--- Google/KMITL-Logo-Replace-Google.user.js
+++ Google/KMITL-Logo-Replace-Google.user.js
@@ -3,14 +3,17 @@
 // @namespace   github.com/phuwit.KMITL-Logo-Replace.Google
 // @match       https://*.google.com/*
 // @grant       none
-// @version     1.1
+// @version     1.1.1
 // @author      https://github.com/phuwit
 // @description haha funny
 // @downloadURL https://cdn.jsdelivr.net/gh/phuwit/KMITL-Logo-Replace@main/Google/KMITL-Logo-Replace-Google.user.js
 // @homepageURL https://github.com/phuwit/KMITL-Logo-Replace
 // ==/UserScript==
 
+// pick one: black and white, or color
 let new_logo = "https://cdn.jsdelivr.net/gh/phuwit/KMITL-Logo-Replace@main/Google/image/kmitl_logo_bw.min.svg";
+// let new_logo = "https://cdn.jsdelivr.net/gh/phuwit/KMITL-Logo-Replace@main/Google/image/kmitl_logo_color.min.svg";
+
 let old_logo_id = "100206365847567318724"
 
 function replaceSrc(srcValue) {
