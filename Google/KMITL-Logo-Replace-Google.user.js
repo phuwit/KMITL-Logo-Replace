78715fe7c43af18d0491a37174ce05cde6cdeaa9
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2023-09-29 11:18:29 +0700

    feat: "improve" matching again, now ppl should config before using

--- Google/KMITL-Logo-Replace-Google.user.js
+++ Google/KMITL-Logo-Replace-Google.user.js
@@ -3,46 +3,49 @@
 // @namespace   github.com/phuwit.KMITL-Logo-Replace.Google
 // @match       https://*.google.com/*
 // @grant       none
-// @version     1.3
+// @version     1.4
 // @author      https://github.com/phuwit
 // @description haha funny
 // @downloadURL https://raw.githubusercontent.com/phuwit/KMITL-Logo-Replace/main/Google/KMITL-Logo-Replace-Google.user.js
 // @homepageURL https://github.com/phuwit/KMITL-Logo-Replace
 // ==/UserScript==
 
-// pick one: black and white, or color
-let new_logo = "https://raw.githubusercontent.com/phuwit/KMITL-Logo-Replace/main/Google/image/kmitl_logo_bw.svg";
-// let new_logo = "https://raw.githubusercontent.com/phuwit/KMITL-Logo-Replace/main/Google/image/kmitl_logo_color.svg";
-let old_logo_id = "100206365847567318724"
+// EDIT THIS AREA VVV
 
-function replaceSrc(srcValue) {
-    let element
+    // pick one: black and white, or color
+    const new_logo = "https://raw.githubusercontent.com/phuwit/KMITL-Logo-Replace/main/Google/image/kmitl_logo_bw.svg";
+    // const new_logo = "https://raw.githubusercontent.com/phuwit/KMITL-Logo-Replace/main/Google/image/kmitl_logo_color.svg";
 
-    // probably less intensive matching via classname
-    element = document.getElementsByClassName("gb_Hc gb_Ja gb_Ma");
+    // google user id
+    // EXAMPLE : 
+    // from https://drive.google.com/drive/u/1/my-drive
+    // ------------------------------------^^^---------
+    // google user id would be 1, so you would enter
+    // const google_user_id = "1";
+
+    // if there are no user id present, enter an empty string
+    // const google_user_id = "";
+
+    const google_user_id = "1";
     
-    if (element.length > 0) {
-        for (let i = 0; i < element.length; i++) {
-            if (element[i].hasAttribute("src")) {
-                if (element[i].getAttribute("src").search(old_logo_id) != -1) {
-                    element[i].src = new_logo;
-                    break;
-                }
-            }
+// EDIT THIS AREA ^^^
+
+const old_logo_filename = "logo.gif";
+const old_logo_service_query = "service=google_gsuite";
+const old_logo_user_path = "u/" + google_user_id;
+
+// fallback matching method
+element = document.getElementsByTagName("img");
+
+for (let i = 0; i < element.length; i++) {
+    if (element[i].hasAttribute("src")) {
+        let src = element[i].getAttribute("src");
+        if (src.search(old_logo_filename) != -1 &&
+            src.search(old_logo_service_query) != -1 &&
+            src.search(old_logo_user_path) != -1
+            ) {
+            element[i].src = new_logo;
+            break;
         }
     }
-    
-    // // fallback matching method
-    // element = document.getElementsByTagName("img");
-
-    // for (let i = 0; i < element.length; i++) {
-    //     if (element[i].hasAttribute("src")) {
-    //         if (element[i].getAttribute("src").search(old_logo_id) != -1) {
-    //             element[i].src = new_logo;
-    //             break;
-    //         }
-    //     }
-    // }
 }
-
-replaceSrc(old_logo_id);
